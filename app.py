5c4f9ee94f1c9de30b610b42d01be579669a8804
liberty-1776 <hritikgl518@gmail.com>
2025-09-14 23:06:03 +0530

    Test

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ search=DuckDuckGoSearchRun(name="Search")
 st.title("🔎 LangChain - Chat with search")
 """
 In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
-Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
+Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agents).
 """
 
 ## Sidebar for settings
